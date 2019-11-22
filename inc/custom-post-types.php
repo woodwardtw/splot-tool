b5826dea0157100905e4c94be02c44d29e1a97db
Tom Woodward <twwoodward@me.com>
2019-11-21 21:42:26 -0500

    single splot layout, acf improvements

--- inc/custom-post-types.php
+++ inc/custom-post-types.php
@@ -144,3 +144,36 @@ function create_review_cpt() {
 }
 add_action( 'init', 'create_review_cpt', 0 );
 
+
+
+// hook into the init action and call create_book_taxonomies when it fires
+add_action( 'init', 'create_splot_taxonomies', 0 );
+
+// create two taxonomies, genres and writers for the post type "book"
+function create_splot_taxonomies() {
+    // Add new taxonomy, make it hierarchical (like categories)
+    $labels = array(
+        'name'              => _x( 'Type', 'taxonomy general name', 'textdomain' ),
+        'singular_name'     => _x( 'Type', 'taxonomy singular name', 'textdomain' ),
+        'search_items'      => __( 'Search Types', 'textdomain' ),
+        'all_items'         => __( 'All Types', 'textdomain' ),
+        'parent_item'       => __( 'Parent Type', 'textdomain' ),
+        'parent_item_colon' => __( 'Parent Type:', 'textdomain' ),
+        'edit_item'         => __( 'Edit Type', 'textdomain' ),
+        'update_item'       => __( 'Update Type', 'textdomain' ),
+        'add_new_item'      => __( 'Add New Type', 'textdomain' ),
+        'new_item_name'     => __( 'New Type Name', 'textdomain' ),
+        'menu_name'         => __( 'Type', 'textdomain' ),
+    );
+
+    $args = array(
+        'hierarchical'      => true,
+        'labels'            => $labels,
+        'show_ui'           => true,
+        'show_admin_column' => true,
+        'query_var'         => true,
+        'rewrite'           => array( 'slug' => 'type' ),
+    );
+
+    register_taxonomy( 'type', array( 'splot' ), $args );
+}
